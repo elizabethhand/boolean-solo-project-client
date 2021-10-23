81db8a8cc775762ddd243c4a11c532c3a5fdd56b
Lizzie Hand <elizabethghand@gmail.com>
2021-10-23 20:51:33 +0100

    Render homepage map

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react"
 import DealTile from "../components/Deal-tile"
 import CategoryTile from "../components/Category-tile";
+import ReactMapGL, {
+    Marker,
+    Popup
+} from "react-map-gl";
 
 export default function Homepage() {
     const [categories, setCategories] = useState([])
     const [deals, setdeals] = useState([])
+    const [viewport, setViewport] = useState({
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+    });
 
     useEffect(() => {
         fetch('http://localhost:3030/categories')
@@ -16,11 +27,40 @@ export default function Homepage() {
             .then(data => setdeals(data.data))
     }, []);
 
+    const MAPBOX_TOKEN = 'pk.eyJ1IjoibGl6emllaGFuZCIsImEiOiJja3Y0NnEwdG0yYXBzMzFxdzRyc3hrdW1lIn0.43AQ7KfSybeTpzMJl_RuZA';
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+    function success(pos) {
+        var crd = pos.coords;
+        console.log(crd.latitude)
+        console.log(crd.longitude)
+        setViewport({
+            ...viewport,
+            latitude: crd.latitude,
+            longitude: crd.longitude
+        })
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    function renderMarkers() {
+
+    }
 
     if (categories && deals) {
         return (
             <div>
-                <div className="map"></div>
+                <ReactMapGL
+                    {...viewport}
+                    width="360px"
+                    height="300px"
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    onViewportChange={setViewport}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                />
                 <h2 className="category-title"> Categories</h2>
                 <div className="tile-container">
                     {categories.map((category) => (
