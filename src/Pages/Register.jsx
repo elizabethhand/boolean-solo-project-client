dd7dd5be97aa0be7b974c61904ea64598400c800
Lizzie Hand <elizabethghand@gmail.com>
2021-10-23 19:20:18 +0100

    Build register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,13 +1,43 @@
 import React from "react"
 import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
+import { useHistory } from "react-router-dom";
 
 export default function Register() {
+    let history = useHistory();
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const form = event.target
+
+        let loginData = {
+            firstName: form.firstName.value,
+            lastName: form.lastName.value,
+            email: form.email.value,
+            username: form.username.value,
+            password: form.password.value
+        }
+
+        fetch("http://localhost:3030/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+            credentials: "include",
+        }).then(() => {
+            history.push("/");
+        });
+    }
+
+
+
     return (
         <main className="main-register-div">
             <div className="signup-container">
                 <h1 className="signup-title"> Sign up</h1>
-                <form className="signup-form">
+                <form className="signup-form" onSubmit={handleSubmit}>
                     <TextField
                         className="input"
                         variant="outlined"
